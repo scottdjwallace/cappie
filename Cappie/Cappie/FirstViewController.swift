5abed1fde0e2da8e19c164310f02c02e2fa00bb7
Scott Wallace <ecosse_18@hotmail.com>
2017-07-29 20:36:27 -0400

    Initial Project Commit

---
+++ Cappie/Cappie/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Cappie
+//
+//  Created by Scott Wallace on 2017-07-29.
+//  Copyright © 2017 Scott Wallace. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
